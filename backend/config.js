cf4210e83ea037d7991b6d56246b1271a195eaa2
AdieDev0 <devutella@gmail.com>
2025-01-05 21:05:33 +0800

    Saved Book

--- backend/config.js
+++ backend/config.js
@@ -2,4 +2,5 @@
 export const PORT = 7777;
 
 // MONGO DB
-export const mongoDBURL = 'mongodb+srv://login:UtellaAdobo@adie.fy9ntbq.mongodb.net/book-shelf-mern/retryWrites=true&w=majority&appName=adie'
+export const mongoDBURL =
+  "mongodb+srv://login:UtellaAdobo@adie.fy9ntbq.mongodb.net/bookshelf?retryWrites=true&w=majority&appName=adie";
