c4c2bc2a29b0b2f659062fc7f79b04d7d38751da
AdieDev0 <devutella@gmail.com>
2025-01-05 19:11:21 +0800

    setup (BACKEND)

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,2 @@
+
 export const PORT = 7777;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 import express from "express";
-import { PORT } from "./config";
+import { PORT } from "./config.js";
 const app = express();
+
+app.get('/', (request, repsonse)=>{
+    console.log(request)
+    return response.status(234).send('Welcome')
+})
+
+app.listen(PORT, () => {
+  console.log(`Hello World: ${PORT}`);
+});
