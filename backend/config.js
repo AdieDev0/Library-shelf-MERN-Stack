b1c0aa22bb740fa11a74c9679fa60f67f5dc3b13
AdieDev0 <devutella@gmail.com>
2025-01-05 19:19:13 +0800

    Setup Database (BACKEND)

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,5 @@
-
+// PORT
 export const PORT = 7777;
+
+// MONGO DB
+export const mongoDBURL = 'mongodb+srv://login:UtellaAdobo@adie.fy9ntbq.mongodb.net/book-shelf-mern/retryWrites=true&w=majority&appName=adie'
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
 const app = express();
 
-app.get('/', (request, repsonse)=>{
-    console.log(request)
-    return response.status(234).send('Welcome')
-})
-
-app.listen(PORT, () => {
-  console.log(`Hello World: ${PORT}`);
+app.get("/", (request, repsonse) => {
+  console.log(request);
+  return response.status(234).send("Welcome");
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("APP CONNECTED TO DATABASE");
+    app.listen(PORT, () => {
+      console.log(`Hello World: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
