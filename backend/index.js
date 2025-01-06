b75b2b90fdc61e4d518f2b38e4e469514aaf9332
AdieDev0 <devutella@gmail.com>
2025-01-06 22:32:21 +0800

    ROUTE GET ALL THE BOOKS

--- backend/index.js
+++ backend/index.js
@@ -41,9 +41,16 @@ app.post("/books", async (request, response) => {
   }
 });
 
-
 // GET ALL THE BOOKS ROUTE
-
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json(books);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 mongoose
   .connect(mongoDBURL)
   .then(() => {
