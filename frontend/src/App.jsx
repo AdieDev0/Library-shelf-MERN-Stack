bd612c14d7afbfd758de849d0eed08b17b8452c7
AdieDev0 <devutella@gmail.com>
2025-01-07 16:46:40 +0800

    React Router Componens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import CreateBooks from "./Components/CreateBooks";
+import DeleteBook from "./Components/DeleteBook";
+import ShowBook from "./Components/ShowBook";
+import EditBook from "./Components/EditBook";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return;
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/books/create" element={<CreateBooks />} />
+    <Route path="/books/delete/:id" element={<DeleteBook />} />
+    <Route path="/books/details/:id" element={<ShowBook />} />
+    <Route path="/books/edit/:id" element={<EditBook />} />
+  </Routes>;
+};
 
-export default App
+export default App;
