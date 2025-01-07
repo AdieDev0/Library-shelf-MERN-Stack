1bcb167d77aecd61b9dd46702cc0b8efad1a8f63
AdieDev0 <devutella@gmail.com>
2025-01-07 11:16:40 +0800

    Get one book by id

--- backend/index.js
+++ backend/index.js
@@ -57,6 +57,21 @@ app.get("/books", async (request, response) => {
   }
 });
 
+// GET ONE BOOK BY ID
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json({
+      // ADDED DATA LOOKS FORMAT LIKE 1,2,3,4
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 // NPM START
 mongoose
   .connect(mongoDBURL)
