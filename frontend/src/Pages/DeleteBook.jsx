4cfc8b2951e2460dc9168e2ec1f208ad0588ec30
AdieDev0 <devutella@gmail.com>
2025-01-09 19:50:07 +0800

    Delete Book API

--- frontend/src/Pages/DeleteBook.jsx
+++ frontend/src/Pages/DeleteBook.jsx
@@ -6,8 +6,39 @@ import { useNavigate, useParams } from "react-router-dom";
 
 const DeleteBook = () => {
   const [loading, setLoading] = useState(false);
-
-  return <div>Delete Book</div>;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  // HandleDelete
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:7777/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("Check error happend. please check the console");
+        console.log(error);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">Are you sure you want to delete this book?</h3>
+        <button
+          onClick={handleDeleteBook}
+          className="p-4 bg-red-600 text-white m-8 w-full"
+        >
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteBook;
